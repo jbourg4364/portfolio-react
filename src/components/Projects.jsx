ed2d4cf5e84ad6b073473a96a5ad0d6b27fe88bc
jbourg4364 <jasonjbourg22@gmail.com>
2023-04-27 18:14:41 -0500

    altered projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,8 +17,8 @@ const Projects = () => {
                     </div>
                     <p>Take a trip to the moon with this interactive version of the classic game of Tic-Tac-Toe. The game randomly chooses the first player and determines a winner accordingly. With a reset of the state board, the players never have to refresh the page.</p>
                     <div className='label-container'>
-                        <button className='label-end'><a className='label-end' href='https://exquisite-pony-390f59.netlify.app/'>See Live</a></button>
-                        <button className='label-end'><a className='label-end' href='https://exquisite-pony-390f59.netlify.app/'>Source Code</a></button>
+                        <a className='label-end' href='https://exquisite-pony-390f59.netlify.app/'>See Live</a>
+                        <a className='label-end' href='https://github.com/jbourg4364/tic-tac-toe/'>Source Code</a>
                     </div>
                     <img className='project-image' src={Images.galaxy} alt='' />
                 </div>
