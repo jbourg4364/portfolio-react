4720fa936d9563eabec748f82089238ad5d64dc9
jbourg4364 <jasonjbourg22@gmail.com>
2023-04-29 12:40:02 -0500

    added react reveal

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import { Header, Welcome, Skills, Projects, About, Contact } from './';
 import './App.css';
+import Reveal from 'react-reveal/Reveal';
+
 
 const App = () => {
 
+
+    
     return (
         <>
-        <div>
-            <Header />
-            <Welcome />
-            <Skills />
-            <Projects />
-            <About />
-            <Contact />
-        </div>
+            <div>
+                <Header /> 
+                <Reveal>
+                    <Welcome /> 
+                    <Skills />
+                    <Projects />
+                    <About />
+                    <Contact />
+                </Reveal>
+            </div>
         </>
         
     )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Welcome.css';
+import { Transition } from 'react-transition-group';
 
 const Welcome = () => {
     return (
